ffe09a20cf8e414a0d85f8ad10f45a9a52170d35
jaderosse <jademrosse@gmail.com>
2018-05-08 19:45:21 -0700

    debugging server side but front end is golden

---
+++ README.md
@@ -0,0 +1,5 @@
+## Sites Used
+
+https://medium.com/@daspinola/video-stream-with-node-js-and-html5-320b3191a6b6
+
+http://blog.adeel.io/2017/07/13/streaming-html5-video-through-node-js/
--- server.js
+++ server.js
@@ -4,11 +4,41 @@ var path = require('path');
 var pug = require('pug');
 var app = express();
 
+// var  = fs.createStreamSample(TearsOfSteel.mp4, {start, end});
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get('/vid', function(req, res){
+	var range = req.headers.range;
+	var stat = fs.statSync('TearsOfSteel.mp4', function(err, stats){
+		if(err){
+			if(err.code === 'ENOENT'){
+				res.status(404);
+			}
+		}
+	});
+	if(!range){
+		res.status(416);
+	}
+	var fileSize = stat.size;
+	var file = fs.createReadStream('TearsOfSteel.mp4', {start, end});
+	var positions = range.replace(/bytes=/, "").split("-");
+	var start = parsenInt(positions[0], 10);
+	var end = positions[1] ? parsenInt(positions[1], 10) : fileSize - 1;
+	var chunkSize = (end-start)+1;
+	var head = {
+		"Content-Range": "bytes " + start + "-" + end + "/" + total,
+		"Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
 });
 
 app.listen(3000);
