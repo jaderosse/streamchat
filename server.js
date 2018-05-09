dba5c40eb055974042056522ddbaebb294fc0785
jaderosse <jademrosse@gmail.com>
2018-05-08 21:00:53 -0700

    made a dumb webcam sample vid for test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 
 app.get('/vid', function(req, res){
 	var range = req.headers.range;
-	var stat = fs.statSync('TearsOfSteel.mp4', function(err, stats){
+	var stat = fs.statSync('sample_video.mp4', function(err, stats){
 		if(err){
 			if(err.code === 'ENOENT'){
 				res.status(404);
@@ -24,7 +24,7 @@ app.get('/vid', function(req, res){
 		res.status(416);
 	}
 	var fileSize = stat.size;
-	var file = fs.createReadStream('TearsOfSteel.mp4', {start, end});
+	var file = fs.createReadStream('sample_video.mp4', {start, end});
 	var positions = range.replace(/bytes=/, "").split("-");
 	var start = parsenInt(positions[0], 10);
 	var end = positions[1] ? parsenInt(positions[1], 10) : fileSize - 1;
