a1f6871d4dc6f22a9da9389f9a685747b92a8109
jaderosse <jademrosse@gmail.com>
2018-05-08 21:33:10 -0700

    added a couple modules and something is rendering but not the video i want

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var pug = require('pug');
+var http = require('http');
+var url = require('url');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -15,28 +17,37 @@ app.get('/vid', function(req, res){
 	var range = req.headers.range;
 	var stat = fs.statSync('sample_video.mp4', function(err, stats){
 		if(err){
+			console.log('error', err);
 			if(err.code === 'ENOENT'){
 				res.status(404);
 			}
 		}
 	});
-	if(!range){
-		res.status(416);
-	}
 	var fileSize = stat.size;
-	var file = fs.createReadStream('sample_video.mp4', {start, end});
-	var positions = range.replace(/bytes=/, "").split("-");
-	var start = parsenInt(positions[0], 10);
-	var end = positions[1] ? parsenInt(positions[1], 10) : fileSize - 1;
-	var chunkSize = (end-start)+1;
-	var head = {
-		"Content-Range": "bytes " + start + "-" + end + "/" + total,
-		"Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4"
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
+	if(!range){
+		console.log('no range');
+		var head = {
+			"Content-Length": fileSize,
+			"Content-Type": "video/mp4"
+		}
+		res.writeHead(200, head);
+		fs.createReadStream('sample_video.mp4').pipe(res);
+	} else {
+		console.log('else like there is range');
+		var positions = range.replace(/bytes=/, "").split("-");
+		var file = fs.createReadStream('sample_video.mp4', {start, end});
+		var start = parseInt(positions[0], 10);
+		var end = positions[1] ? parseInt(positions[1], 10) : fileSize - 1;
+		var chunkSize = (end-start)+1;
+		var head = {
+			"Content-Range": "bytes " + start + "-" + end + "/" + fileSize,
+			"Accept-Ranges": "bytes",
+	        "Content-Length": chunkSize,
+	        "Content-Type": "video/mp4"
+	    }
+	    res.writeHead(206, head);
+	    file.pipe(res);
+	}
 });
 
 app.listen(3000);
